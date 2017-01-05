8d125f76bac40b1065c698f0240cb6fa9b8d460a
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2017-01-05 12:59:28 -0200

    Update sections

--- resources/views/home/section.blade.php
+++ resources/views/home/section.blade.php
@@ -1,16 +1,25 @@
 @extends('templates.default')
 
 @section('content')
-    <div class="row text-center conteudo">
-        <div class="col-md-12 text-center">
-            <h1>Portal da Transparência</h1>
-        </div>
+    <div class="row">
+        <div class="row text-center">
+            <div class="col-md-12 text-center">
+                <h1>Portal da Transparência</h1>
+            </div>
 
-        <div class="col-md-offset-2 col-md-8 text-center">
-            <div class="box">
-                <div class="box-content">
-                    <h2>{{ $section['title'] }}</h2>
+            <div class="col-md-offset-2 col-md-8 text-center">
+                <div class="box">
+                    <div class="box-content">
+                        <h2>{{ $section['title'] }}</h2>
 
+                        <img class="tag-icon" src="{{ $section['icon'] }}">
+
+                        <ul>
+                            @foreach ($section['links'] as $link)
+                                <li><a href="{{ $link['link'] }}">{{ $link['title'] }}</a></li>
+                            @endforeach
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
