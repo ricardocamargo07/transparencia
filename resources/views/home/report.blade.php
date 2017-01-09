9a92d11718ec5c5240793264c5d5a2b3a2d4027e
brenot <brenot@gmail.com>
2017-01-09 13:47:31 +0000

    Adequação Section Report LAyout

--- resources/views/home/report.blade.php
+++ resources/views/home/report.blade.php
@@ -6,40 +6,41 @@
             <h2>{{ $report['section']['title'] }}</h2>
         </div>
         <div class="col-md-offset-2 col-md-8 text-center">
-            <div class="">
+            <div class="box">
                 <div class="box-content">
-                    <h3>{{ $report['title'] }}</h3>
+                    <h3 class="tag-title">{{ $report['title'] }}</h3>
 
-                    @if ($report['body'])
-                        <div class="introduction">
-                            {!! $report['body'] !!}
-                        </div>
-                    @endif
-
-                    @if ($report['files']->count())
-                        <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-                            @foreach ($report['files'] as $year => $files)
-                                <div class="panel panel-default">
-                                    <div class="panel-heading" role="tab" id="heading1">
-                                        <h4 class="panel-title">
-                                            <a data-toggle="collapse" data-parent="#accordion" href="#collapse1" aria-expanded="true" aria-controls="collapse1">
-                                                {{ $year }}
-                                            </a>
-                                        </h4>
-                                    </div>
-                                    <div id="collapse1" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="heading1">
-                                        <div class="panel-body">
-                                            @foreach ($files as $file)
-                                                <a href="{{ $file['url'] }}">{{ $file['title'] }}</a>
-                                            @endforeach
-                                        </div>
-                                    </div>
-                                </div>
-                            @endforeach
-                        </div>
-                    @endif
+                    <div class="">
+                        @if ($report['body'])
+                            <div class="introduction">
+                                {!! $report['body'] !!}
+                            </div>
+                        @endif
+                    </div>
                 </div>
             </div>
+            @if ($report['files']->count())
+                <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+                    @foreach ($report['files'] as $year => $files)
+                        <div class="panel panel-default">
+                            <div class="panel-heading" role="tab" id="heading1">
+                                <h4 class="panel-title">
+                                    <a data-toggle="collapse" data-parent="#accordion" href="#collapse1" aria-expanded="true" aria-controls="collapse1">
+                                        {{ $year }}
+                                    </a>
+                                </h4>
+                            </div>
+                            <div id="collapse1" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="heading1">
+                                <div class="panel-body">
+                                    @foreach ($files as $file)
+                                        <a href="{{ $file['url'] }}">{{ $file['title'] }}</a>
+                                    @endforeach
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+            @endif
         </div>
 
         <div class="row">
