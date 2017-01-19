11f0f0ccc2951c2a50f8721b7b407f431b9d9767
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2017-01-19 12:15:13 -0200

    Change text

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -9,13 +9,13 @@
 
                 <div class="intro01">
                     <p>A Assembleia Legislativa do Estado do Rio de Janeiro (Alerj), honrando sua tradição de pioneirismo, foi a primeira casa legislativa do país e a primeira instituição pública do estado a lançar seu Portal da Transparência, em 21 de maio de 2012.</p>
-                    <p>A medida atende às determinações da Lei 12.527/2011, de Acesso à Informação, de 18 de novembro de 2011, em vigor desde maio de 2012.</p>
+                    <p>A medida atende às determinações da Lei 12.527/2011, a Lei de Acesso à Informação, de 18 de novembro de 2011, em vigor desde maio de 2012.</p>
                 </div>
 
                 <div class="intro2">
-                    <p>A <a href="http://www.planalto.gov.br/ccivil_03/_ato2011-2014/2011/lei/l12527.htm" target="_blank">LAI</a> permite que qualquer pessoa física ou jurídica requisite informações públicas – um direito constitucional – sem que precise apresentar motivo para tal. A norma vale para os três Poderes da União, Estados, Municípios e Distrito Federal, inclusive Tribunais de Contas e Ministério Público. Engloba, ainda, entidades privadas que recebam recursos públicos.</p>
-                    <p>A Alerj entende que Transparência vai muito além da mera disponibilização de dados. Ela passa também pela efetiva participação, acompanhamento e fiscalização por parte da sociedade para o exercício pleno da cidadania. E isso só é possível a partir do acesso das pessoas à informação.</p>
-                    <p>Informação é Poder. E, na Alerj, você tem poder!</p>
+                    <p>A <a href="http://www.planalto.gov.br/ccivil_03/_ato2011-2014/2011/lei/l12527.htm" target="_blank">LAI</a> permite que qualquer pessoa física ou jurídica requisite informações públicas – um direito constitucional – sem que precise apresentar motivo para tal. A norma vale para os três Poderes da União, estados, municípios e Distrito Federal, inclusive Tribunais de Contas e Ministério Público. Engloba, ainda, entidades privadas que recebam recursos públicos.</p>
+                    <p>A Alerj entende que transparência vai muito além da mera disponibilização de dados. Ela passa também pela efetiva participação, acompanhamento e fiscalização da sociedade para o exercício pleno da cidadania. E isso só é possível a partir do acesso das pessoas à informação.</p>
+                    <p>Informação é poder. E, na Alerj, você tem poder!</p>
                 </div>
             </div>
 
