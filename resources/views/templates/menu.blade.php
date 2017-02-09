d564521b4f67c7c0dd057b6b02880708743b7864
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2017-02-09 14:12:14 -0200

    Disable headers while on App mode

---
+++ resources/views/templates/menu.blade.php
@@ -0,0 +1,118 @@
+<div class="base_menu">
+    <a href="javascript:;" class="quadrado"></a>
+    <div class="menu_movel">
+        <div class="controle">
+            <div class="menu_select">
+                <div class="descricao"><li><a href="/" id="lkbInicioMovel">Início</a></li></div>
+                <div class="icone Expandir">
+                    <a href="javascript:;" title="Expandir Menu">
+                        <img src="http://www.alerj.rj.gov.br/Content/imagens/icone_21.png" width="40" height="34" alt="Expandir Menu" title="Expandir Menu" border="0">
+                    </a>
+                </div>
+                <div class="icone Recuar" style="display: none;">
+                    <a href="javascript:;" title="Recolher Menu">
+                        <img src="http://www.alerj.rj.gov.br/Content/imagens/icone_22.png" width="40" height="34" alt="Recolher Menu" title="Recolher Menu" border="0">
+                    </a>
+                </div>
+            </div>
+            <div class="posicao_menu_movel" style="display: none;">
+                <div class="fundo_submovel">
+                    <ul>
+                        <li><a href="/" id="lkbInicioMovel">Início</a></li>
+
+                        <li class="pai">
+                            <a target="" href="javascript:;" class="titulo" title="ALERJ">Alerj <span class="caret"></span></a>
+
+                            <div class="submenu_movel">
+                                <ul>
+                                    <li>
+                                        <a target="" href="http://www.alerj.rj.gov.br/Alerj/ComoFunciona" title="Como Funciona">Como Funciona</a>
+                                    </li>
+                                    <li>
+                                        <a target="" href="http://www.alerj.rj.gov.br/Alerj/MesaDiretora" title="Mesa Diretora">Mesa Diretora</a>
+                                    </li>
+                                    <li>
+                                        <a target="" href="http://www.alerj.rj.gov.br/Alerj/Historia" title="História">História</a>
+                                    </li>
+                                    <li>
+                                        <a target="" href="http://www.alerj.rj.gov.br/Alerj/VisitaGuiada" title="Visita Guiada">Visita Guiada</a>
+                                    </li>
+                                    <li>
+                                        <a target="" href="http://www.alerj.rj.gov.br/Alerj/TourVirtual" title="Tour Virtual">Tour Virtual</a>
+                                    </li>
+                                    <li>
+                                        <a target="" href="http://www.alerj.rj.gov.br/Alerj/Biblioteca" title="Biblioteca">Biblioteca</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+
+                        <li><a href="http://local.legislaqui.com/about" target="" class="titulo">Como Funciona?</a></li>
+
+                        <li class="pai">
+                            <a target="" href="javascript:;" class="titulo" title="Ideia Legislativa">Ideia Legislativa <span class="caret"></span></a>
+
+                            <div class="submenu_movel">
+                                <ul>
+                                    <li><a target="" href="http://local.legislaqui.com/proposals/create">Proponha uma ideia legislativa logando ou registrando-se aqui</a>
+                                    </li>
+                                    <li role="separator" class="divider"></li>
+                                    <li><a href="http://local.legislaqui.com/terms">Termos de Uso</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+
+                        <li><a href="/contact" class="titulo " title="Contato">Contato</a></li>
+
+                        <!-- Authentication Links -->
+
+                        <li>
+                            <a href="http://local.legislaqui.com/login" class="titulo" title="Login | Registro">Login | Registro</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="detalhe"></div>
+            </div>
+        </div>
+    </div>
+
+    <div class="menu">
+        <ul>
+            <li><a class="ativo" href="/" id="lkbInicio">INÍCIO</a></li>
+
+            <li class="pai">
+                <a href="/about" target="" class="titulo">Contato</a>
+            </li>
+
+        {{--  <li class="pai">
+              <a href="#" class="titulo" title="Ideia Legislativa">Ideia Legislativa <span class="caret"></span></a>
+
+              <div class="submenu" style="display: none;">
+                  <ul>
+                      <li>
+                          <div><a href="http://local.legislaqui.com/proposals/create">Proponha uma ideia legislativa logando ou registrando-se aqui</a>
+                          </div>
+                      </li>
+                      <li role="separator" class="divider"></li>
+                      <li>
+                          <div><a href="http://local.legislaqui.com/terms">Termos de Uso</a>
+                          </div>
+                      </li>
+                  </ul>
+              </div>
+          </li>--}}
+
+        <!-- relationship commissions -->
+        {{--    <li class="pai"><a href="/committee" class="titulo " title="Comissões">Nossas comissões</a></li>
+            <li class="pai"><a href="/terms" class="titulo " title="Comissões">Termos de uso</a></li>
+            <li class="pai"><a href="/contact" class="titulo " title="Contato">Contato</a></li>--}}
+
+        <!-- Authentication Links -->
+
+            {{--<li class="pai">
+                <a href="http://local.legislaqui.com/login" class="titulo" title="Login | Registro">Login | Registro</a>
+            </li>--}}
+        </ul>
+    </div>
+</div>
---
+++ resources/views/templates/superior.blade.php
@@ -0,0 +1,46 @@
+<div class="superior">
+    <div class="brilho ">
+        <div class="conteudo">
+            <div class="logotipo">
+                <a href="http://www.alerj.rj.gov.br/">
+                    <img src="http://www.alerj.rj.gov.br/Content/imagens/logotipo.png" class="sem_altocontraste" width="210" alt="Assembléia Legislativa do Estado do Rio de Janeiro" title="Assembléia Legislativa do Estado do Rio de Janeiro">
+                    <img src="http://www.alerj.rj.gov.br/Content/imagens/logotipo_altocontraste.png" width="210" class="altocontraste" alt="Assembléia Legislativa do Estado do Rio de Janeiro" title="Assembléia Legislativa do Estado do Rio de Janeiro">
+                </a>
+            </div>
+
+            <div class="coluna pull-right">
+                <div class="redes_sociais">
+                    <div class="logotipo_redes">
+                        <a target="_blank" href="https://instagram.com/instalerj/" title="Instagram">
+                            <img src="http://www.alerj.rj.gov.br/Content/imagens/icone_36.png" width="20" height="20" alt="Instagram" title="Instagram">
+                        </a>
+                    </div>
+
+                    <div class="logotipo_redes">
+                        <a target="_blank" href="https://www.facebook.com/assembleiaRJ" title="Facebook">
+                            <img src="http://www.alerj.rj.gov.br/Content/imagens/icone_06.png" width="20" height="20" alt="Facebook" title="Facebook">
+                        </a>
+                    </div>
+
+                    <div class="logotipo_redes">
+                        <a target="_blank" href="https://plus.google.com/u/0/+alerj/posts" title="Google Mais">
+                            <img src="http://www.alerj.rj.gov.br/Content/imagens/icone_07.png" width="20" height="20" alt="Google Mais" title="Google Mais">
+                        </a>
+                    </div>
+
+                    <div class="logotipo_redes">
+                        <a target="_blank" href="https://twitter.com/alerj" title="Twitter">
+                            <img src="http://www.alerj.rj.gov.br/Content/imagens/icone_08.png" width="20" height="20" alt="Twitter" title="Twitter">
+                        </a>
+                    </div>
+
+                    <div class="logotipo_redes">
+                        <a target="_blank" href="https://www.youtube.com/user/dcsalerj" title="Youtube">
+                            <img src="http://www.alerj.rj.gov.br/Content/imagens/icone_09.png" width="20" height="20" alt="Youtube" title="Youtube">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
