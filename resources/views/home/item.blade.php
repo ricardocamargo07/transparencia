efbb530645b0409c73ef7152b2f45ea8615f74d1
Breno Trengrouse <brenot@gmail.com>
2017-08-17 18:13:21 +0000

    kill ghost div 2

--- resources/views/home/item.blade.php
+++ resources/views/home/item.blade.php
@@ -7,17 +7,17 @@
 @section('content')
     <div class="row">
         <div class="col-md-offset-1 col-md-10 text-center">
-            <div class="">
-                <div class="box-content-item">
-                    <h3 class="">{{ $item['title'] }}</h3>
 
-                    @if ($body = $item['html'] ?: $item['body'])
-                        <div class="introduction">
-                            {!! $body !!}
-                        </div>
-                    @endif
-                </div>
+            <div class="box-content-item">
+                <h3 class="">{{ $item['title'] }}</h3>
+
+                @if ($body = $item['html'] ?: $item['body'])
+                    <div class="introduction">
+                        {!! $body !!}
+                    </div>
+                @endif
             </div>
+
 
             @if (isset($item['files']) && $item['files']->count())
                 <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
@@ -35,7 +35,7 @@
                                     @foreach ($files as $group)
                                         <div class="row linha-mes">
                                             <div class="col-xs-5 col-md-offset-3 col-md-3 mes-label">
-                                               {{ $group[0]['title'] }}
+                                                {{ $group[0]['title'] }}
                                             </div>
 
                                             <div class="col-xs-7 col-md-3 tipos-downloads">
